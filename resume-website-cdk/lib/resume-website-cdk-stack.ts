f9ce5d35ddd75ce1e2c7f65a2caa629d57472131
Sarah Wadley <lifeunsubscribe@gmail.com>
2025-07-13 01:28:11 -0600

    added cute domain and ACM verification 🫶🏼

--- resume-website-cdk/lib/resume-website-cdk-stack.ts
+++ resume-website-cdk/lib/resume-website-cdk-stack.ts
@@ -3,6 +3,9 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
@@ -10,13 +13,32 @@ export class ResumeWebsiteCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const rootDomain = 'cloudwithsarah.com';
+    const wwwDomain = 'www.cloudwithsarah.com';
+
     // S3 Bucket for hosting the website
     const websiteBucket = new s3.Bucket(this, 'ResumeWebsiteBucket', {
+      bucketName: wwwDomain,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
       objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       enforceSSL: true,
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: 'index.html',
+      publicReadAccess: false,
+    });
+
+    // Redirect bucket for root domain
+    const redirectBucket = new s3.Bucket(this, 'RedirectBucket', {
+      bucketName: rootDomain,
+      websiteRedirect: {
+        hostName: wwwDomain,
+        protocol: s3.RedirectProtocol.HTTPS
+      },
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      enforceSSL: true,
       publicReadAccess: false,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
@@ -28,7 +50,19 @@ export class ResumeWebsiteCdkStack extends cdk.Stack {
       destinationBucket: websiteBucket
     });
 
-    // Add this right after bucket creation
+    // Look up hosted zone
+    const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+      domainName: rootDomain
+    });
+
+    // Certificate covering both root and www domains
+    const certificate = new acm.Certificate(this, 'Certificate', {
+      domainName: rootDomain,
+      subjectAlternativeNames: [wwwDomain],
+      validation: acm.CertificateValidation.fromDns(hostedZone)
+    });
+
+    // CFN OAC (Origin Access Control) for CloudFront to access the private S3 bucket
     const oac = new cloudfront.CfnOriginAccessControl(this, 'SiteDistributionOAC', {
       originAccessControlConfig: {
         name: 'StaticSiteOAC',
@@ -42,15 +76,29 @@ export class ResumeWebsiteCdkStack extends cdk.Stack {
     // Then create the distribution with OAC
     const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
       defaultRootObject: 'index.html',
+      domainNames: [rootDomain, wwwDomain],
       defaultBehavior: {
         origin: cloudfrontOrigins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
+      certificate: certificate,
     });
 
     // Manually associate OAC with the distribution
     const cfnDistribution = distribution.node.defaultChild as cloudfront.CfnDistribution;
     cfnDistribution.addPropertyOverride('DistributionConfig.Origins.0.OriginAccessControlId', oac.getAtt('Id'));
+
+    // Route53 records
+    new route53.ARecord(this, 'RootRecord', {
+      zone: hostedZone,
+      recordName: rootDomain,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
+    });
+    new route53.ARecord(this, 'WwwRecord', {
+      zone: hostedZone,
+      recordName: "www",
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
+    });
 
     // Update bucket policy to allow OAC
     websiteBucket.addToResourcePolicy(new iam.PolicyStatement({
@@ -64,13 +112,16 @@ export class ResumeWebsiteCdkStack extends cdk.Stack {
       }
     }));
 
+    // Outputs
     new cdk.CfnOutput(this, 'BucketName', {
       value: websiteBucket.bucketName,
       description: 'S3 Bucket Name',
     });
-
     new cdk.CfnOutput(this, 'DistributionDomain', {
       value: distribution.distributionDomainName
+    });
+    new cdk.CfnOutput(this, 'SiteUrl', {
+      value: `https://${wwwDomain}`
     });
 
   }
