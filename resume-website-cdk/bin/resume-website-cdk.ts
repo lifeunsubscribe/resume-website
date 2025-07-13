b27934c069ea3fed130f0d74169d5fbd451288bd
Sarah Wadley <lifeunsubscribe@gmail.com>
2025-07-13 17:26:46 -0600

    attempting ci/cd implementation with automatic s3 deployment on git push 🤞

--- resume-website-cdk/bin/resume-website-cdk.ts
+++ resume-website-cdk/bin/resume-website-cdk.ts
@@ -2,14 +2,19 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ResumeWebsiteCdkStack } from '../lib/resume-website-cdk-stack';
+// filepath: /Users/sarahtime/Dev/resume-website/resume-website-cdk/bin/resume-website-cdk.ts
+import { GitHubActionsRoleStack } from '../lib/github-actions-role-stack';
 
 const app = new cdk.App();
 new ResumeWebsiteCdkStack(app, 'ResumeWebsiteCdkStack', {
-
   env: { account: '342587863995', region: 'us-east-1' },
   description: 'Stack for hosting resume website on S3 with CloudFront',
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
+new GitHubActionsRoleStack(app, 'GitHubActionsRoleStack', {
+  env: { account: '342587863995', region: 'us-east-1' },
+  description: 'CI/CD Stack for role in GitHub Actions to automatically deploy to S3',
+
 });
 
 // Add tags to all resources in the stack
---
+++ resume-website-cdk/lib/github-actions-role-stack.ts
@@ -0,0 +1,69 @@
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import { Construct } from 'constructs';
+
+export class GitHubActionsRoleStack extends cdk.Stack {
+    public readonly githubActionsRole: iam.Role;
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // Your S3 bucket (replace with your actual bucket ARN)
+        const resumeBucket = s3.Bucket.fromBucketArn(
+            this,
+            'ResumeBucket',
+            'arn:aws:s3:::www.cloudwithsarah.com'
+        );
+
+        // GitHub OIDC Provider (already exists in AWS, but we reference it)
+        const githubOidcProvider = iam.OpenIdConnectProvider.fromOpenIdConnectProviderArn(
+            this,
+            'GitHubOidcProvider',
+            'arn:aws:iam::342587863995:oidc-provider/token.actions.githubusercontent.com'
+        );
+
+        // IAM Role for GitHub Actions
+        this.githubActionsRole = new iam.Role(this, 'GitHubActionsRole', {
+            assumedBy: new iam.WebIdentityPrincipal(
+                githubOidcProvider.openIdConnectProviderArn,
+                {
+                    StringEquals: {
+                        'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
+                    },
+                    StringLike: {
+                        // Restrict to your GitHub repo (format: repo:org/repo:ref)
+                        'token.actions.githubusercontent.com:sub': [
+                            'repo:lifeunsubscribe/resume-website:*',            // All branches/tags
+                            'repo:lifeunsubscribe/resume-website:pull_request', // PRs
+                        ],
+                    },
+                }
+            ),
+            description: 'Role assumed by GitHub Actions for S3 deployments',
+        });
+
+        // Grant S3 deployment permissions
+        resumeBucket.grantReadWrite(this.githubActionsRole);
+
+        // Look up your existing CloudFront distribution
+        const distribution = cloudfront.Distribution.fromDistributionAttributes(this, 'ResumeDistribution', {
+            distributionId: 'EG3WGB7ERFXYG',
+            domainName: 'cloudwithsarah.com',
+        });
+
+        // Optional: Add CloudFront invalidation permissions
+        this.githubActionsRole.addToPolicy(
+            new iam.PolicyStatement({
+                actions: ['cloudfront:CreateInvalidation'],
+                resources: [`arn:aws:cloudfront::342587863995:distribution/${distribution.distributionId}`],
+            })
+        );
+
+        // Output the Role ARN for GitHub Secrets
+        new cdk.CfnOutput(this, 'GitHubActionsRoleArn', {
+            value: this.githubActionsRole.roleArn,
+        });
+    }
+}
